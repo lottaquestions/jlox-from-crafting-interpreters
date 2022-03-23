5656ad85ae06d463409cb52c4aaa58cfc561ef51
lottaquestions <nickothieno@yahoo.com>
2022-03-23 07:09:20 -0400

    Completion of chapter 5 - Representing Code

---
+++ src/com/craftinginterpreters/lox/RPNPrinter.java
@@ -0,0 +1,40 @@
+package com.craftinginterpreters.lox;
+
+
+public class RPNPrinter implements Expr.Visitor<String>{
+    String print (Expr expr){
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return RPNify(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return RPNify("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return RPNify(expr.operator.lexeme, expr.right);
+    }
+
+    private String RPNify(String name, Expr... exprs){
+        StringBuilder builder = new StringBuilder();
+
+        for (Expr expr : exprs){
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(name);
+        return builder.toString();
+    }
+}
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -5,6 +5,9 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
+// To run this code generator, go to the tools directory and do:
+// java GenerateAst.java ~/IdeaProjects/Lox/src/com/craftinginterpreters/lox
+
 public class GenerateAst {
     public static void main (String[] args) throws IOException{
         if (args.length != 1){
@@ -29,14 +32,30 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST Classes
         for (String type : types){
             String className = type.split(":")[0].trim();
             String fields =  type.split(":")[1].trim();
             defineType (writer, baseName, className, fields);
         }
+
+        // The base accept() method
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor (PrintWriter writer, String baseName, List<String> types){
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types){
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("    }");
     }
 
     private static void defineType (PrintWriter writer, String baseName, String className, String fieldList){
@@ -45,12 +64,19 @@ public class GenerateAst {
         // Constructor
         writer.println("        " + className + "(" + fieldList + ") {");
 
-        //Store parameters in fields
+        // Store parameters in fields
         String[] fields = fieldList.split(", ");
         for(String field : fields){
             String name = field.split(" ")[1];
             writer.println("                this." + name + " = " + name + ";");
         }
+        writer.println("        }");
+
+        // Visitor pattern
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
         writer.println("        }");
 
         // Fields
